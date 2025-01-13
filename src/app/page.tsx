548a2d23c12f096374e5b3c5dc461f98473c21f1
usuhuu18 <dev@erxes.io>
2025-01-13 15:38:29 +0800

    up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import FullScreenImage from "../components/FullScreenImage";
-// import Container from "../components/Container";
-import Products from "../components/Products";
+import Container from "../components/Container";
+// import Products from "../components/Products";
 import CommentContainer from "../components/CommentContainer";
 import About from "../components/About";
 import Contact_Info from "../components/Contact_Info";
@@ -11,8 +11,8 @@ export default function Home({ searchParams }: IPageProps) {
   return (
     <div>
       <FullScreenImage />
-      {/* <Container /> */}
-      <Products searchParams={searchParams} params={{}} />
+      <Container />
+      {/* <Products searchParams={searchParams} params={{}} /> */}
       <CommentContainer />
       <About />
       <Contact_Info />
